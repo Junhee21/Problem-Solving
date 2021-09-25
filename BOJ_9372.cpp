8aede1dc29d93fbf6b1d37145a5a47330ab35a80
Junhee <wwoon63@snu.ac.kr>
2021-09-25 10:33:02 +0900

    상근이의 여행

---
+++ BOJ_9372.cpp
@@ -0,0 +1,27 @@
+//21 09 25
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(void)
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    int test_num;
+    cin>>test_num;
+
+    while (test_num--)
+    {
+        int n, m;
+        cin>>n>>m;
+        while(m--)
+        {
+            int a, b;
+            cin>>a>>b;
+        }
+        cout<<n-1<<"\n";
+    }
+    return 0;
+}
