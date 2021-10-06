d6fe86ad19e80149443cc51482652a8a65d0d601
Junhee <wwoon63@snu.ac.kr>
2021-10-06 09:57:42 +0900

    검증수

---
+++ BOJ_2475.cpp
@@ -0,0 +1,13 @@
+//21 10 06
+
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+    int a, b, c, d, e;
+    cin>>a>>b>>c>>d>>e;
+    cout<<((a*a)+(b*b)+(c*c)+(d*d)+(e*e))%10;
+
+    return 0;
+}
