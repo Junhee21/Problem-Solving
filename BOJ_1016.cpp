71e8bae18c2b9b918b6f13aeb0072db8d55ca8e0
Junhee <wwoon63@snu.ac.kr>
2021-10-25 12:05:21 +0900

    제곱 ㄴㄴ 수

---
+++ BOJ_1016.cpp
@@ -0,0 +1,42 @@
+// 21/10/22
+
+#include <iostream>
+using namespace std;
+
+long long minnum, maxnum;
+bool check[1000000+1];
+bool prime[1000000+1000];
+
+void Solve(void)
+{
+    for (long long i=2; 1; i++)
+    {
+        if (i*i > maxnum)
+            break;
+        if (prime[i])
+            continue;
+        for (long long j=i; j<=1001000; j+=i)
+            prime[j] = true;
+        long long temp = minnum/(i*i);
+        for (long long j=temp*(i*i); j<=maxnum; j+=(i*i))
+        {
+            if (j<minnum)
+                continue;
+            check[j-minnum] = true;
+        }
+    }
+}
+
+int main(void)
+{
+    cin >> minnum >> maxnum;
+    Solve();
+    int answer = 0;
+    for (long long i=0; i<=(maxnum-minnum); i++)
+    {
+        if (!check[i])
+            answer ++;
+    }
+    cout << answer;
+    return 0;
+}
