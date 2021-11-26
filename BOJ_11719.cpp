27d35d92143e2b2707578e0b80995184d0399bb6
Junhee <wwoon63@snu.ac.kr>
2021-11-26 15:12:43 +0900

    그대로 출력하기 2

---
+++ BOJ_11719.cpp
@@ -0,0 +1,13 @@
+// 21/11/26
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(void)
+{
+    string s;
+    while (getline(cin, s))
+        cout << s << "\n";
+    return 0;
+}
