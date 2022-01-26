ce7fb79dc65f870b7ff893c00fac8a3434f83355
Junhee <wwoon63@snu.ac.kr>
2022-01-26 11:45:54 +0900

    별 찍기 - 12

---
+++ BOJ_2522.cpp
@@ -0,0 +1,26 @@
+// 22 / 1 / 26
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(void)
+{
+    int n;
+    cin >> n;
+
+    for (int i = 1; i < 2 * n; i++)
+    {
+        int x;
+        if (i <= n)
+            x = i;
+        else
+            x = 2 * n - i;
+        for (int j = 0; j < n - x; j++)
+            cout << " ";
+        for (int j = 0; j < x; j++)
+            cout << "*";
+        cout << "\n";
+    }
+    return 0;
+}
