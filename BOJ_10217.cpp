a530ff7b6a921b8e0fe614230cfa8ab6f5fb08f9
junhee-won <wwoon63@snu.ac.kr>
2023-08-08 04:07:45 +0900

    KCM Travel - 다익스트라 풀이

--- BOJ_10217.cpp
+++ BOJ_10217.cpp
@@ -1,5 +1,6 @@
 #include <algorithm>
 #include <iostream>
+#include <queue>
 #include <vector>
 #define INF 987654321
 using namespace std;
@@ -13,24 +14,21 @@ struct Edge {
   }
 };
 
+struct Q {
+  int node, cost, time;
+  Q(int node, int cost, int time) {
+    this->node = node;
+    this->cost = cost;
+    this->time = time;
+  }
+  bool operator<(const Q &second) const { return time > second.time; }
+};
+
 int t, n, m, k, a, b, c, d;
 vector<vector<int>> dp;
 vector<vector<Edge *>> edges;
 
-int dfs(int node, int cost) {
-  if (node == n)
-    return 0;
-  if (dp[node][cost] != -1)
-    return dp[node][cost];
-  dp[node][cost] = INF;
-  for (auto &iter : edges[node]) {
-    if (cost + iter->cost > m)
-      continue;
-    dp[node][cost] =
-        min(dp[node][cost], dfs(iter->e, cost + iter->cost) + iter->time);
-  }
-  return dp[node][cost];
-}
+bool compare(Edge *a, Edge *b) { return a->time < b->time; }
 
 int main(void) {
   ios_base::sync_with_stdio(0);
@@ -38,18 +36,64 @@ int main(void) {
   cin >> t;
   while (t--) {
     cin >> n >> m >> k;
-    dp.assign(n + 1, vector<int>(m + 1, -1));
+
+    dp.assign(n + 1, vector<int>(m + 1, INF));
     edges.resize(n + 1);
     for (int i = 0; i < k; i++) {
       cin >> a >> b >> c >> d;
       edges[a].push_back(new Edge(b, c, d));
     }
 
-    int result = dfs(1, 0);
-    if (result == INF)
+    for (int i = 0; i <= m; i++) {
+      dp[1][i] = 0;
+    }
+
+    for (int i = 1; i <= n; i++) {
+      sort(edges[i].begin(), edges[i].end(), compare);
+    }
+
+    priority_queue<Q> pq;
+    pq.push({1, 0, 0});
+
+    while (!pq.empty()) {
+      int start_time = pq.top().time;
+      int start_cost = pq.top().cost;
+      int start_node = pq.top().node;
+      if (start_node == n)
+        break;
+      pq.pop();
+
+      if (dp[start_node][start_cost] < start_time)
+        continue;
+
+      for (int i = 0; i < edges[start_node].size(); i++) {
+        int next_time = start_time + edges[start_node][i]->time;
+        int next_cost = start_cost + edges[start_node][i]->cost;
+        int next_node = edges[start_node][i]->e;
+
+        if (next_cost > m)
+          continue;
+        if (dp[next_node][next_cost] > next_time) {
+          for (int j = next_cost; j <= m; j++) {
+            if (dp[next_node][j] <= next_time) {
+              break;
+            }
+            dp[next_node][j] = next_time;
+          }
+          dp[next_node][next_cost] = next_time;
+          pq.push({next_node, next_cost, next_time});
+        }
+      }
+    }
+    int min = INF;
+    for (int i = 0; i <= m; i++) {
+      if (dp[n][i] < min)
+        min = dp[n][i];
+    }
+    if (min == INF)
       cout << "Poor KCM\n";
     else
-      cout << result << "\n";
+      cout << min << "\n";
   }
   return 0;
 }
