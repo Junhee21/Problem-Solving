6f48c64eb0c73b45b806e7dddb54ba2f4554e16f
junhee-won <wwoon63@snu.ac.kr>
2023-08-08 03:47:33 +0900

    KCM Travel

--- BOJ_10217.cpp
+++ BOJ_10217.cpp
@@ -1,65 +1,55 @@
-// 21 09 01 > 21 09 02 > 21 09 04
-// 다익스트라 알고리즘 사용
-
-#include <stdio.h>
+#include <algorithm>
+#include <iostream>
 #include <vector>
-#include <queue>
 #define INF 987654321
 using namespace std;
 
+struct Edge {
+  int e, time, cost;
+  Edge(int e, int cost, int time) {
+    this->e = e;
+    this->cost = cost;
+    this->time = time;
+  }
+};
+
 int t, n, m, k, a, b, c, d;
-int dp[101][10001];
+vector<vector<int>> dp;
+vector<vector<Edge *>> edges;
 
-int main(void){
-    scanf("%d", &t);
-    while (t--){
-        scanf("%d %d %d", &n, &m, &k);
-        
-        vector<pair<int, pair<int, int>>> line[n+1];
-        while (k--){
-            scanf("%d %d %d %d", &a, &b, &c, &d);
-            line[a].push_back(make_pair(b, make_pair(c,d)));
-        }
-
-        for (int i=1; i<=n; i++){
-            for (int j=0; j<=m; j++)
-                dp[i][j] = INF;
-        }
-
-        dp[1][0] = 0;
-        priority_queue<pair<int, pair<int, int>>>pq;
-        pq.push({0,{0,1}});
-
-        while (!pq.empty()){
-            int start_time = -pq.top().first;
-            int start_cost = pq.top().second.first;
-            int start_node = pq.top().second.second;
-            pq.pop();
-
-            if (dp[start_node][start_cost] < start_time)    continue;
-            for (int i=0; i<line[start_node].size(); i++){
-                int next_time = start_time + line[start_node][i].second.second;
-                int next_cost = start_cost + line[start_node][i].second.first;
-                int next_node = line[start_node][i].first;
-
-                if (next_cost > m)  continue;
-                if (dp[next_node][next_cost] > next_time){
-                    for (int j=next_cost; j<=m; j++){
-                        if (dp[next_node][j] > next_time)
-                            dp[next_node][j] = next_time;
-                    }
-                    pq.push({-next_time, {next_cost, next_node}});
-                }
-            }
-        }
-        int min=INF;
-        for(int i=0; i<=m; i++){
-            if (dp[n][i] < min)
-                min = dp[n][i];
-        }
-
-        if (min==INF)   printf("Poor KCM\n");
-        else    printf("%d\n", min);
-    }
+int dfs(int node, int cost) {
+  if (node == n)
     return 0;
+  if (dp[node][cost] != -1)
+    return dp[node][cost];
+  dp[node][cost] = INF;
+  for (auto &iter : edges[node]) {
+    if (cost + iter->cost > m)
+      continue;
+    dp[node][cost] =
+        min(dp[node][cost], dfs(iter->e, cost + iter->cost) + iter->time);
+  }
+  return dp[node][cost];
+}
+
+int main(void) {
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+  cin >> t;
+  while (t--) {
+    cin >> n >> m >> k;
+    dp.assign(n + 1, vector<int>(m + 1, -1));
+    edges.resize(n + 1);
+    for (int i = 0; i < k; i++) {
+      cin >> a >> b >> c >> d;
+      edges[a].push_back(new Edge(b, c, d));
+    }
+
+    int result = dfs(1, 0);
+    if (result == INF)
+      cout << "Poor KCM\n";
+    else
+      cout << result << "\n";
+  }
+  return 0;
 }
